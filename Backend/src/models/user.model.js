1bb8c3b299e008180e8f2f2fc2bd5db8e74bd5c4
Divam Sanghvi <divamsanghvi@gmail.com>
2024-10-28 17:12:48 +0530

    added auth middleware and changed user schema

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const UserSchema = new mongoose.Schema({
     fullname : {
@@ -46,4 +48,43 @@ const UserSchema = new mongoose.Schema({
     },
 },{timestamps : true
 })
+
+UserSchema.pre("save",async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password ,10);
+    next()
+})
+
+UserSchema.methods.isPasswordCorrect=async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+UserSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            fullname : this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model('User',UserSchema);
